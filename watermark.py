f9d302324edf3d07d8fd91f65d46a98f048afdbe
Cil Satriawan <cil.satriawan@gmail.com>
2018-09-21 04:09:22 +0700

    Write videos in FFV1 format (uncompressed) to preserve watermark

--- watermark.py
+++ watermark.py
@@ -2,10 +2,14 @@
 import os
 import numpy as np
 import imageio as iio
+import skvideo.io as skv
 import matplotlib
+import logging
 from argparse import ArgumentParser
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+
+log = logging.getLogger(__name__)
 
 
 class Watermark:
@@ -17,10 +21,6 @@ class Watermark:
         self.arn_x = None
         self.arn_y = None
         self.key = key
-
-    def read_image(self, image_path):
-        # read image as ndarray
-        return iio.imread(image_path)
 
     def pad_image(self, image):
         if image.shape[0] == image.shape[
@@ -159,15 +159,6 @@ class Watermark:
         else:
             return xor
 
-    def encrypt_4d(self, src, n_trans=0):
-        """
-        Runs encryption on videos. Assumes a square matrix divisible by 4.
-        """
-        # Stack images
-        vid = np.stack(src)
-        vid[:] = np.bitwise_and(vid, ~1)
-
-
     def gif_encrypt(self, src, dest=None):
         ext = os.path.splitext(src)[-1]
         if ext in ['.gif', '.mp4']:
@@ -194,6 +185,94 @@ class Watermark:
             if not self.show:
                 iio.imwrite(dest, img)
 
+    def encrypt_vid(self, src, dest):
+        """
+        Runs encryption on videos. Assumes a square matrix divisible by 4.
+        """
+        vid = skv.vread(src)
+        # set LSB to 0
+        vid[:] = np.bitwise_and(vid, ~1)
+        binary = self.binarize_4d(vid)
+        self.arnold_4d(binary, n=len(vid))
+        # embed watermark
+        log.info('Embedding watermark and saving')
+        vid[:] = np.bitwise_or(vid, binary)
+        params = {'-c:v': 'ffv1'}
+        with skv.FFmpegWriter(dest, outputdict={'-c:v': 'ffv1'}) as f:
+            [f.writeFrame(vid[n]) for n in range(len(vid))]
+
+    def decrypt_vid(self, src, dest):
+        vid = skv.vread(src)
+        # extract LSB and then set to 0
+        lsb = np.bitwise_and(vid, 1).astype(bool)
+        vid[:] = np.bitwise_and(vid, ~1)
+        binary = self.binarize_4d(vid)
+        self.arnold_4d(lsb, n=(192 - len(vid)))
+        # localize
+        xor = np.bitwise_xor(binary, lsb)
+        params = {'-c:v': 'ffv1'}
+        with skv.FFmpegWriter(dest, outputdict={'-c:v': 'ffv1'}) as f:
+            [f.writeFrame(xor[n] * 192) for n in range(len(vid))]
+
+    def tamper_vid(self, src, dest, n_tamper=20):
+        vid = skv.vread(src)
+        t, x, y, _ = vid.shape
+        rand_t = np.random.randint(t, size=n_tamper)
+        rand_x = np.random.randint(x, size=n_tamper)
+        rand_y = np.random.randint(y, size=n_tamper)
+        rand_z = np.random.randint(255, size=n_tamper)
+        for n in range(n_tamper):
+            vid[rand_t[n], rand_x[n]:rand_y[n], rand_x[n]:
+                rand_y[n], :] = rand_z[n]
+        with skv.FFmpegWriter(dest, outputdict={'-c:v': 'ffv1'}) as f:
+            [f.writeFrame(vid[n]) for n in range(len(vid))]
+
+    def binarize_4d(self, vid):
+        assert len(vid.shape) == 4
+        log.info('Segmenting into blocks of size %d' % self.blocksize)
+        frames, x, y, colors = vid.shape
+        blksz = self.blocksize
+        pixsz = vid.strides[-1]
+        block_shape = (frames, x // blksz, y // blksz, blksz, blksz, colors)
+        _, a, b, c, d, e = block_shape
+        stride_size = (a * b * c * d * e * pixsz, b * c * d * e * pixsz,
+                       c * d * e * pixsz, d * e * pixsz, e * pixsz, pixsz)
+        blocks = np.lib.stride_tricks.as_strided(
+            vid, shape=block_shape, strides=stride_size)
+        log.info('Computing SVD')
+        lambdas = np.linalg.svd(blocks, full_matrices=False, compute_uv=False)
+        log.info('Thresholding')
+        lambda1 = lambdas[:, :, :, 0]
+        lambda1[lambda1 == 0] = self.blocksize
+        divisor = (1 / lambda1).reshape(
+            (lambda1.shape[0], lambda1.shape[1], lambda1.shape[2], 1, 1,
+             lambda1.shape[-1]))
+        binary = (divisor * blocks) >= self.threshold
+        return np.lib.stride_tricks.as_strided(binary, shape=vid.shape)
+
+    def arnold_4d(self, vid, n=0):
+        log.info('Calculating Arnold transform')
+        x, y = np.meshgrid(
+            np.arange(vid.shape[1]), np.arange(vid.shape[2]), indexing='ij')
+        if n != self.transforms or self.arn_x is None or self.arn_y is None:
+            tmp_x = x.copy()
+            tmp_y = y.copy()
+            arn_x = np.empty(x.shape, x.dtype)
+            arn_y = np.empty(y.shape, y.dtype)
+            for _ in range(n):
+                arn_x[:] = (2 * tmp_x + tmp_y) % vid.shape[1]
+                arn_y[:] = (tmp_x + tmp_y) % vid.shape[2]
+                tmp_x[:] = arn_x
+                tmp_y[:] = arn_y
+            self.arn_x = arn_x
+            self.arn_y = arn_y
+            self.transforms = n
+        else:
+            tmp_x = self.arn_x
+            tmp_y = self.arn_y
+        if n > 0:
+            vid[:, tmp_x, tmp_y] = vid[:, x, y]
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Implements fragile watermarking")
@@ -210,18 +289,25 @@ if __name__ == '__main__':
     parser.add_argument(
         '-t', '--threshold', help="Binarization threshold", default=0.25)
     parser.add_argument(
+        '-m', '--mode', help="Mode to operate in: encrypt, decrypt, tamper",
+        default='encrypt'
+    )
+    parser.add_argument(
         '--show', action='store_true', help="Show binary image")
     parser.add_argument(
-        '-k', '--key', help="Number of arnold transforms to perform",
-        default=64
-    )
+        '-k',
+        '--key',
+        help="Number of arnold transforms to perform",
+        default=64)
     args = parser.parse_args()
 
     if not args.output:
         args.show = True
-    wm = Watermark(int(args.blocksize), float(args.threshold), args.show,
-                   int(args.key))
-    if args.decrypt:
-        wm.gif_decrypt(args.input, args.output)
-    else:
-        wm.gif_encrypt(args.input, args.output)
+    wm = Watermark(
+        int(args.blocksize), float(args.threshold), args.show, int(args.key))
+    if args.mode == 'encrypt':
+        wm.encrypt_vid(args.input, args.output)
+    elif args.mode == 'decrypt':
+        wm.decrypt_vid(args.input, args.output)
+    elif args.mode == 'tamper':
+        wm.tamper_vid(args.input, args.output)
